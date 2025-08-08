853a01bed85589a34d670a3befad058a5f24810c
MhFikri <fikrianwar9345@gmail.com>
2025-08-08 15:46:37 +0700

    solved problem 68 (sqrt x)

---
+++ easy/69 (Sqrt x)/solution.py
@@ -0,0 +1,11 @@
+import math
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        b = math.sqrt(x)
+        c = int(b)
+        return c
+        
